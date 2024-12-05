1ab60cf083f9c6ef0859266f29cb7dfc35ab192d
Asjad Khan <160101352+thehexatown-asjad@users.noreply.github.com>
2024-12-05 20:09:18 +0500

    Change RapidApi key

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
 const WEATHER_API_URL = 'https://open-weather13.p.rapidapi.com/city';
-const RAPIDAPI_KEY = '2947f99970msh11549719a27e940p17d807jsnc11cc6a1bf8e'; // Replace with your RapidAPI key
+const RAPIDAPI_KEY = '0b8bc10b4dmsh6c5519bba475faep1f1c38jsnc0969d81fc26';
 const RAPIDAPI_HOST = 'open-weather13.p.rapidapi.com';
 export {WEATHER_API_URL, RAPIDAPI_KEY, RAPIDAPI_HOST};
